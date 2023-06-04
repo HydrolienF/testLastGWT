784d5a73ebca2b691d1bb32b088c9e9ffb61d29c
HydrolienF <hydrolien.f@gmail.com>
2023-06-04 12:24:05 +0200

    Center app on web browser by removing weel & padding.

--- html/src/fr/formiko/testlastgwt/client/HtmlLauncher.java
+++ html/src/fr/formiko/testlastgwt/client/HtmlLauncher.java
@@ -4,13 +4,20 @@ import fr.formiko.testlastgwt.LastGWT;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.GwtGraphics;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig() {
                 // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
+                GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                // return new GwtApplicationConfiguration(1600, 900);
+                cfg.fullscreenOrientation = GwtGraphics.OrientationLockType.LANDSCAPE;
+                cfg.padHorizontal = 0;
+                cfg.padVertical = 0;
+                return cfg;
         }
 
         @Override
